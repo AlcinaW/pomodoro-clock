d9fe5a5b5d9488ac1a598a16ce744fa57e02c7a4
AlcinaW <white_kaitou@hotmail.com>
2016-02-15 20:06:43 -0500

    Revert "Initial commit"
    This reverts commit 06e6e6a32dbc9689bcc09c3ec1f3bbefd07123bc.

--- app.js
+++
@@ -1,9 +0,0 @@
-window.onload = setInterval(function () {
-    var d = new Date();
-    var seconds = d.getMinutes() * 60 + d.getSeconds(); 
-    var twoMin = 60 * 2; //2 minutes to test
-    var timeleft = twoMin - seconds % twoMin; 
-    var result = parseInt(timeleft / 60) + ':' + timeleft % 60; 
-    document.getElementById('countdownNum').innerHTML = result;
-
-}, 500);
