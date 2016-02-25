8e2b48f44da2f0e0dc591313fe7bae1824dcae5e
AlcinaW <white_kaitou@hotmail.com>
2016-02-25 00:09:34 -0500

    Started adding numbers to the page

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 //when page loads make the number of the work and break time 25 and 5 
-window.onload = document.getElementById('work_session').innerHTML = 25;
-window.onload = document.getElementById('break_session').innerHTML = 5;
+//if the page is loaded, why not just make it 25 + 5 at start
+// window.onload = document.getElementById('work_session').innerHTML = 25;
+// window.onload = document.getElementById('break_session').innerHTML = 5;
+
+// length of work is equal to HTML atm, which is 25
+var worksession_length = document.getElementById('work_session').innerHTML;
+// break is 5, as in HTML
+var break_length = document.getElementById('break_session').innerHTML;
+
+console.log(worksession_length);
+console.log(break_length);
+
+// up and down for work and break session adjustment 
+var counter = 0; 
+
+//read BOTH + and -, go up or down depending on which is clicked 
+//One function to rule them all
+function incrementMinutes() {
+	
+}
 
