41ad176a88fd7b23361758a8db53149c04ea7e18
AlcinaW <white_kaitou@hotmail.com>
2016-02-27 15:50:31 -0500

    Button incrementing

--- app.js
+++ app.js
@@ -4,20 +4,36 @@
 // window.onload = document.getElementById('break_session').innerHTML = 5;
 
 // length of work is equal to HTML atm, which is 25
-var worksession_length = document.getElementById('work_session').innerHTML;
+var worksession_length = document.getElementById('work_session').value;
 // break is 5, as in HTML
-var break_length = document.getElementById('break_session').innerHTML;
+var break_length = document.getElementById('break_session').value;
+//ALL inputs can't go below 1
 
 console.log(worksession_length);
 console.log(break_length);
 
 // up and down for work and break session adjustment 
-var counter = 0; 
+function increase(){
+	//but why have to write in doc.getElement again? 
+	var value = parseInt(document.getElementById('work_session').value, 10);
+	value = isNaN(value) ? 0 : value;
+	value++;
+	document.getElementById('work_session').value =  value;
+}
+function decrease(){ 
+	var value = parseInt(document.getElementById('work_session').value, 10);
+	value = isNaN(value) ? 0 : value;
+	value--;
+	document.getElementById('work_session').value =  value;
+}
 
 //read BOTH + and -, go up or down depending on which is clicked 
 //One function to rule them all
-function incrementMinutes() {
 
+// input can't go below zero
+function handleChange(input) {
+	if (input.value < 0) input.value = 0;
+    if (input.value > 10) input.value = 10;
 }
 
 // Start button event listener!
