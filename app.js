8e7043971c6ff7481101fbd5a0efb76c397f47e4
AlcinaW <white_kaitou@hotmail.com>
2016-02-28 22:38:09 -0500

    input

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-//when page loads make the number of the work and break time 25 and 5 
-//if the page is loaded, why not just make it 25 + 5 at start
-// window.onload = document.getElementById('work_session').innerHTML = 25;
-// window.onload = document.getElementById('break_session').innerHTML = 5;
-
 // length of work is equal to HTML atm, which is 25
-var worksession_length = document.getElementById('work_session').value;
-var worksesh = document.getElementById('work_session');
+var worksession_length = document.getElementById('work_session');
 // break is 5, as in HTML
-var break_length = document.getElementById('break_session').value;
+var break_length = document.getElementById('break_session');
 
 //count for inputs
+var input = document.getElementsByTagName('input');
+console.log(input);
 var count = 1;
 
-console.log(worksession_length);
-console.log(break_length);
-
-
-
+console.log(worksession_length.value);
+console.log(break_length.value);
 
 // up and down for work and break session adjustment 
+//read BOTH + and -, go up or down depending on which is clicked 
+//One function to rule them all
 function increase(){
 	//but why have to write in doc.getElement again? 
-	var value = parseInt(worksesh.value, 10);
+	var value = parseInt(input.value, 10);
 	value = isNaN(value) ? 0 : value;
 	value++;
-	worksesh.value =  value;
+	worksession_length.value =  value;
 	console.log(value);
 }
 function decrease(){ 
-	var value = parseInt(worksesh.value, 10);
+	var value = parseInt(worksession_length.value, 10);
 	if (value > 1) {
 	value = isNaN(value) ? 0 : value;
 	value--;
-	worksesh.value =  value;
+	worksession_length.value =  value;
 	console.log(value);
 	}
 }
 
 
-//read BOTH + and -, go up or down depending on which is clicked 
-//One function to rule them all
 
-// input can't go below zero
-// function handleChange(input) {
-// 	if (input.value < 0) input.value = 0;
-// 	if (input.value > 100) input.value = 100;
-// }
 
 // Start button event listener!
 
