e6f23c75259a2902983e0ba95fc81f2a6eb9e52f
AlcinaW <white_kaitou@hotmail.com>
2016-02-28 22:16:38 -0500

    Inputs can't go below 0
    Work session only

--- app.js
+++ app.js
@@ -5,44 +5,50 @@
 
 // length of work is equal to HTML atm, which is 25
 var worksession_length = document.getElementById('work_session').value;
+var worksesh = document.getElementById('work_session');
 // break is 5, as in HTML
 var break_length = document.getElementById('break_session').value;
-//ALL inputs can't go below 1
+
+//count for inputs
+var count = 1;
 
 console.log(worksession_length);
 console.log(break_length);
 
+
+
+
 // up and down for work and break session adjustment 
 function increase(){
 	//but why have to write in doc.getElement again? 
-	var value = parseInt(document.getElementById('work_session').value, 10);
+	var value = parseInt(worksesh.value, 10);
 	value = isNaN(value) ? 0 : value;
 	value++;
-	document.getElementById('work_session').value =  value;
+	worksesh.value =  value;
+	console.log(value);
 }
 function decrease(){ 
-	var value = parseInt(document.getElementById('work_session').value, 10);
+	var value = parseInt(worksesh.value, 10);
+	if (value > 1) {
 	value = isNaN(value) ? 0 : value;
 	value--;
-	document.getElementById('work_session').value =  value;
+	worksesh.value =  value;
+	console.log(value);
+	}
 }
+
 
 //read BOTH + and -, go up or down depending on which is clicked 
 //One function to rule them all
 
 // input can't go below zero
-function handleChange(input) {
-	if (input.value < 0) input.value = 0;
-    if (input.value > 10) input.value = 10;
-}
+// function handleChange(input) {
+// 	if (input.value < 0) input.value = 0;
+// 	if (input.value > 100) input.value = 100;
+// }
 
 // Start button event listener!
 
-//end
-
-function doSomething(button) {
-    // do something with button
-}
 
 //display time in minutes/seconds
 
