4ef07136221f15b7fdcf39b17b03ab6ddb81e915
AlcinaW <white_kaitou@hotmail.com>
2016-02-29 22:50:49 -0500

    Update

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var worksession_length = document.getElementById('work_session');
 console.log(worksession_length.value);
 // break is 5, as in HTML
+
 var break_length = document.getElementById('break_session');
 console.log(break_length.value);
 
+// controls status of timer, if stopped or started
 var startStop = document.getElementById("start-stop");
 console.log(startStop.value);
 
@@ -17,30 +19,56 @@ var count = 1;
 var countDown = 60 * worksession_length.value; // minutes number convert to seconds
 console.log(countDown);
 
+//button text swaps when pressed
+function swapText() {
+    if (startStop.value === "Started") {
+    	startStop.value = "Stopped";
+    }
+    else {
+    	startStop.value = "Started";
+    }
+}
 
 // up and down for work and break session adjustment 
-//read BOTH + and -, go up or down depending on which is clicked 
+//To-do: read BOTH + and -, go up or down depending on which is clicked 
 //One function to rule them all
-function increase(){
-	//but why have to write in doc.getElement again? 
-	var value = parseInt(worksession_length.value, 10);
-	value = isNaN(value) ? 0 : value;
-	value++;
-	worksession_length.value = value;
-	console.log(value);
-	document.getElementById("clockTime").innerHTML = value + ":00 minutes";
-}
-function decrease(){ 
-	var value = parseInt(worksession_length.value, 10);
-	if (value > 1) {
-	value = isNaN(value) ? 0 : value;
-	value--;
-	worksession_length.value =  value;
-	console.log(value);
-	document.getElementById("clockTime").innerHTML = value + ":00 minutes";
+// if value === "+" and if value === work
+
+
+
+var plus = document.getElementById("yes");
+var minus = document.getElementById("no");
+
+    yes.onclick = function(){
+        box.style.backgroundColor = "red";
+    }
+
+    no.onclick = function(){
+        box.style.backgroundColor = "green";
+    }
+
+function inputControl(){
+
+	function increase(){
+		//but why have to write in doc.getElement again? 
+		var value = parseInt(worksession_length.value, 10);
+		value = isNaN(value) ? 0 : value;
+		value++;
+		worksession_length.value = value;
+		console.log(value);
+		document.getElementById("clockTime").innerHTML = value + ":00 minutes";
+	}
+	function decrease(){ 
+		var value = parseInt(worksession_length.value, 10);
+		if (value > 1) {
+		value = isNaN(value) ? 0 : value;
+		value--;
+		worksession_length.value =  value;
+		console.log(value);
+		document.getElementById("clockTime").innerHTML = value + ":00 minutes";
+		}
 	}
 }
-
 
 // button that lets you start or stop the timer
 // function startStop() {
@@ -51,15 +79,6 @@ function decrease(){
 //     }
 // }
 
-//button text swaps when pressed
-function swapText() {
-    if (startStop.value === "Started") {
-    	startStop.value = "Stopped";
-    }
-    else {
-    	startStop.value = "Started";
-    }
-}
 
 
 // function secondsToHms(d) {
