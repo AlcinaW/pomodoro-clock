637ca8324ae27b0ecd82c0db3457dd90ff1b74b1
AlcinaW <white_kaitou@hotmail.com>
2016-03-06 10:36:21 -0500

    CSS styling

--- app.js
+++ app.js
@@ -13,25 +13,39 @@ document.getElementById("timerDisplay").innerHTML = workTime;
 
 //SVG start
 
+//sets circle size on load 
+window.onload = function() {
+	var radius = 10, // set the radius of the circle
+    circumference = 2 * radius * Math.PI; 
+  
+  	var els = document.querySelectorAll('circle');
+  	Array.prototype.forEach.call(els, function (el) {
+    el.setAttribute('stroke-dasharray', circumference + 'em');
+    el.setAttribute('r', radius + 'em');
+	});
+}; 
+
+
+// currently on button press
 var player = function() {
 	var radius = 10, // set the radius of the circle
     circumference = 2 * radius * Math.PI; 
 
-  var els = document.querySelectorAll('circle');
-  Array.prototype.forEach.call(els, function (el) {
+  	var els = document.querySelectorAll('circle');
+  	Array.prototype.forEach.call(els, function (el) {
     el.setAttribute('stroke-dasharray', circumference + 'em');
     el.setAttribute('r', radius + 'em');
-  });
+  	});
   
-  document.querySelector('.radial-progress-center').setAttribute('r', (radius - 0.01 + 'em'));
+  	document.querySelector('.radial-progress-center').setAttribute('r', (radius - 0.01 + 'em'));
   
-  var currentCount = 1, 
-      maxCount = remainingSeconds;
+  	var currentCount = 1, 
+      	maxCount = remainingSeconds;
   
-  var intervalId = setInterval(function () { 
+ 	var intervalId = setInterval(function () { 
     if (currentCount > maxCount) {
-      clearInterval(intervalId);
-      return;
+      	clearInterval(intervalId);
+      	return;
     }
     var offset = -(circumference / maxCount) * currentCount + 'em';
     console.log(currentCount, offset);
@@ -41,10 +55,6 @@ var player = function() {
     currentCount++;
   }, 1000);
 }; 
-
-
-
-
 
 
 //controls START button
